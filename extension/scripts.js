eba4b9f0acc23b5d9782d733f1a5c20cb941a16a
lucas-science <lucaslhomme01@gmail.com>
2022-01-14 09:56:48 +0100

    add trash and some css style

--- extension/scripts.js
+++ extension/scripts.js
@@ -10,14 +10,17 @@ const form_time = document.getElementById('form_time');
 let items = []
 
 /* Listeners */
-
+newnode_box.style.display = "none"
 content.addEventListener('click', (e) => {
-    console.log(e.path[0].name)
-    deleteNode(e.path[0].name, items)
+    console.log(e.path[1].name)
+    if(!isNull(e.path[1].name)){
+        deleteNode(e.path[1].name, items)
+    }
 })
 
 NewNodeButton.addEventListener('click', () => {
-    newnode_box.style.display = "block"
+    newnode_box.style.display = "flex"
+    NewNodeButton.style.display = "none"
 })
 
 CreateNodeButton.addEventListener('click', () => {
@@ -28,6 +31,7 @@ CreateNodeButton.addEventListener('click', () => {
     form_url.value = ""
     form_time.value = 0
     newnode_box.style.display = "none"
+    NewNodeButton.style.display = "block"
     InsertAndGetItemsStorage(node, 'list')
 })
 
@@ -35,6 +39,10 @@ chrome.storage.onChanged.addListener(function(changes, namespace) {
     items = changes.list.newValue
     CreateNode(items) // print modifications
 });
+
+const isNull = (val) => {
+    return val == null ? true : false
+}
 
 /**
  * Insert Data in local storage
@@ -85,7 +93,7 @@ const CreateNode = (item) => {
         div.innerHTML = `
             <p>${item[i].url}</p>
             <p>${item[i].time}</p>
-            <button name="${i}">X</button>
+            <button class="button-leave" value="${i}" name="${i}"><img class="trash" src="/baseline_delete_outline_white_24dp.png"></button>
         `;
         document.getElementById('content').appendChild(div);
     }
@@ -106,9 +114,18 @@ const deleteNode = (index, items) => {
         });
     });
 }
-
+chrome.storage.local.get('list', function(result) {
+    console.log(result)
+    if (Object.entries(result).length === 0) {
+        chrome.storage.local.set({ 'list': [] }, () => {
+            chrome.storage.local.get('list', result => console.log(result));
+        })
+    }
+});
 // print items stored
 getItemStorage('list').then(item => {
     items = item
     CreateNode(item)
 })
+
+
